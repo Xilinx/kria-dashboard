4b61ac39e78024da4b0acd60be8c59f3ae5441b5
jues <jues@xilinx.com>
2021-06-28 09:23:16 -0700

    More progress on app_cockpit - added dnf install and dynamic generation oof install buttons. loading app still have temporary template because of CR-1103979

--- app_cockpit.py
+++ app_cockpit.py
@@ -1,30 +1,111 @@
 import subprocess
-from bokeh.models import Button, Paragraph, Div
+from functools import partial
+from bokeh.models import Button, Paragraph, Div, CustomJS
 from bokeh.layouts import layout, row, column, gridplot
 
 import global_var
 
-
 title = Div(
     text="""<h1 style="color :""" + global_var.text_color + """; text-align :center">Kria Zynq MPSoc Application Cockpit</h1>""",
-    width=450)
-
+    width=600)
 
 def xmutil_unloadapp():
-    subprocess.run(['sudo xmutil unloadapp'], shell=True,  capture_output=True)
-unload_button = Button(label="Unloadapp", width=200, button_type='primary')
+    subprocess.run(['sudo xmutil unloadapp'], shell=True, capture_output=True)
+    draw_apps()
+unload_button = Button(label="Unloadapp", width=600, button_type='primary')
 unload_button.on_click(xmutil_unloadapp)
 
-
+#Apps!!!!!###########################################################################################################
 def xmutil_loadapp(app_name):
-    command = str ('sudo xmutil loadapp ' + app_name)
-    print("executing: ", command)
-    subprocess.run(command, shell=True,  capture_output=True)
+    command = str('sudo xmutil loadapp ' + app_name)
+    subprocess.run(command, shell=True, capture_output=True)
+    draw_apps()
+# list out applications - currently listpackage doesnt return stdout correctly, temporarily use a fixed string for dev
+#listapp_output = subprocess.run(['sudo dfx-mgr-client -listPackage | grep kv260'], shell=True, stdout=subprocess.PIPE)
+#print("list app output", listapp_output.stdout)
+load_buttons = []
+active_app_print = Div(
+        text="""<h2 style="color :""" + global_var.text_color + """; text-align :center">Active Accelerator: None</h2>""",
+        width=600)
+def draw_apps():
+    global load_buttons
+    global active_app_print
+    active_app = "None"
+    listapp_output = """                                       Accelerator           Type    Active
+                                        kv260-smartcam       XRT_FLAT         1
+                                              kv260-dp       XRT_FLAT         0
+                                                  base       XRT_FLAT         0"""
+    listapp = listapp_output.split("\n")
+    listapp.pop(0)
+    apps = []
+    load_buttons = []
+    for i in range(len(listapp)):
+        x = listapp[i].split()
+        apps.append(x[0])
+        if x[2] == "1":
+            active_app = x[0]
 
-def load_smartcam():
-    xmutil_loadapp('kv260-smartcam')
-load_button_smartcam = Button(label="Load SmartCam", width=200, button_type='primary')
-load_button_smartcam.on_click(load_smartcam)
+    active_app_print = Div(
+        text="""<h2 style="color :""" + global_var.text_color + """; text-align :center">Active Accelerator: """ + active_app + """</h2>""",
+        width=600)
+
+    for i in range(len(apps)):
+        load_buttons.append(Button(label=apps[i], width=300, button_type='primary'))
+        if active_app !="none":
+            if apps[i] == active_app:
+                load_buttons[i].button_type = 'success'
+                load_buttons[i].js_on_click(CustomJS(code='alert("This Accelerator is already loaded, Unloadapp first!");'))
+            else:
+                load_buttons[i].button_type = 'default'
+                load_buttons[i].js_on_click(CustomJS(code='alert("Unloadapp First!");'))
+        else:
+            load_buttons[i].on_click(partial(xmutil_loadapp, app_name=apps[i]))
+
+draw_apps()
+# packages!!###########################################################################################################
+
+package_print = Div(
+    text="""<h2 style="color :""" + global_var.text_color + """; text-align :center">Packages to Download</h2>""",
+    width=600)
+
+def dnf_install(app_name):
+    command = str('sudo dnf install ' + app_name + " -y")
+    print("execute command: ", command)
+    subprocess.call(command, shell=True)
+    print("finished command: ", command)
+    draw_pkgs()
+    layout2.children[5] = column(pkgs_buttons)
+
+
+pkgs_buttons = []
+def draw_pkgs():
+    global pkgs_buttons
+    #subprocess.run(['sudo dnf update'], shell=True)
+    #subprocess.run(['sudo dnf clean all'], shell=True)
+    getpkgs_output = subprocess.run(['sudo xmutil getpkgs | grep packagegroup-kv260'], shell=True, stdout=subprocess.PIPE).stdout.decode("utf-8")
+    print("getpkgs_output", getpkgs_output)
+    list_pkgs = getpkgs_output.split("\n")
+    pkgs_buttons = []
+    for i in range(len(list_pkgs)-1):
+        x = list_pkgs[i].split()
+        pkgs_buttons.append(Button(label=x[0], width=300, button_type='primary'))
+        pkgs_buttons[i].on_click(partial(dnf_install, app_name=x[0]))
+
+draw_pkgs()
+
+layout2 = layout([
+    [title],
+    [active_app_print],
+    [unload_button],
+    column(load_buttons),
+    [package_print],
+    column(pkgs_buttons),
+])
+layout2.background = global_var.bg_color
+
+
+
+
 def smartcam_mipi_rtsp():
     subprocess.run(['sudo 01.mipi-rtsp.sh'], shell=True)
 run_button_smartcam01 = Button(label="MIPI-RTSP", width=200, button_type='primary')
@@ -33,15 +114,3 @@ def smartcam_mipi_dp():
     subprocess.run(['sudo 02.mipi-dp.sh'], shell=True)
 run_button_smartcam02 = Button(label="MIPI-DP", width=200, button_type='primary')
 run_button_smartcam02.on_click(smartcam_mipi_dp)
-
-def load_aibox():
-    xmutil_loadapp('kv260-aibox-reid')
-load_button_aibox = Button(label="Load AIBox", width=200, button_type='primary')
-load_button_aibox.on_click(load_aibox)
-
-
-layout2 = layout(column(row(title, align='center'),
-                        unload_button,
-                        row(load_button_smartcam, run_button_smartcam01, run_button_smartcam02, background=global_var.bg_color),
-                        load_button_aibox,
-                        background=global_var.bg_color,))
