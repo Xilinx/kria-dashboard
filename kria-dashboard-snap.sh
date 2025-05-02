ff9be6a42d70e253e2eefb1983a1365e30c4db24
Talha Can Havadar <havadartalha@gmail.com>
2025-05-02 08:02:23 +0000

    snap: echo ip address before starting bokeh server

--- kria-dashboard-snap.sh
+++ kria-dashboard-snap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "SOM Dashboard will be running at http://$(hostname -I | cut -d' ' -f1):5006/kria-dashboard"
 $SNAP/bin/bokeh serve --address 0.0.0.0 --allow-websocket-origin=* --show $SNAP/var/opt/kria-dashboard
